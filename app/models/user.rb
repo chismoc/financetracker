a17e3c7ba902c898cf8ff3324dcb5a223e9e43e3
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-11-15 22:38:10 +0200

    add user lastname and firstname fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-def stock_already_tracked?(ticker_symbol)
-  stock = Stock.check_db(ticker_symbol)
-  return false unless stock
-  stocks.where(id: stock.id).exists?
-end
+    def stock_already_tracked?(ticker_symbol)
+      stock = Stock.check_db(ticker_symbol)
+      return false unless stock
+      stocks.where(id: stock.id).exists?
+    end
 
-def under_stock_limit?
-    stocks.count < 7
-end
+    def under_stock_limit?
+        stocks.count < 7
+    end
 
-def can_track_stock?(ticker_symbol)
-  under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-end
+    def can_track_stock?(ticker_symbol)
+      under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+    end
+
+    def full_name
+     return "#{first_name} #{last_name}" if first_name || last_name
+     "Anonymous"
+    end
 end
