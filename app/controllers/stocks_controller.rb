b865274c026ad5d5edfb4852701483df4f6238ca
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-11-15 17:06:09 +0200

    add search and add stock functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,16 @@
 class StocksController < ApplicationController
-    def search
-        if params[:stock].present?
-            @stock = Stock.new_lookup(params[:stock])
-            if @stock
-                render 'users/my_portfolio'
-            else
-                flash[:alert] = "Please enter a valid symbol to search"
-                redirect_to my_portfolio_path
-            end
-        else
-            flash[:alert] = "Please enter a symbol to search"
-            redirect_to my_portfolio_path
-        end
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+         render partial: 'users/result'
+      else
+        flash[:alert] = "This stock symbol does not exist  "
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "You didn't enter any symbol  "
+       redirect_to my_portfolio_path
     end
+  end
 end
